64fa751645f960d87b1c551f7ef68cf1ef5258b0
Lenz Balde <mohamedlaminebalde392@gmail.com>
2024-11-27 18:00:04 +0000

    ajout pipline

--- movie_review/settings.py
+++ movie_review/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'movie_review.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'movie_dev',
+        'USER': 'movie',
+        'PASSWORD':'movie',
+        'HOST': '127.0.0.1',
+        'PORT': 5459
     }
 }
 
